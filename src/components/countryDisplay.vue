b26a8cbf0923dec4f584f81305ac56d95dfa95fc
Jason Kiptoo <kiptoojason@gmail.com>
2023-04-14 15:45:17 +0300

    styled the country display container

--- src/components/countryDisplay.vue
+++ src/components/countryDisplay.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="container">
     <div class="country-container">
-      <h2>Choose Country to View</h2>
+      <h2>Select a Country to View</h2>
+
+
+
+
+
+
+
+    </div>
+    <div class="container-details">
+       <h2>You have selected    </h2> 
     </div>
   </div>
 </template>
@@ -11,9 +21,31 @@ export default {};
 </script>
 
 <style scoped>
-.country-container{
-    position: absolute;
+.container{
+    position: relative;
     left: 0;
-    top: 10px;
+    top: 100px;
+    /* border: 1px solid red; */
+    /* background-color: aquamarine; */
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 1rem;
+
+}
+.country-container{
+ 
+    background-color: aliceblue;
+    border: 2px solid black;
+    border-radius: 10px;
+    height: 100%;
+}
+.container-details{
+    background-color: aliceblue;
+    border: 2px solid black;
+    border-radius: 10px;
+    height: 100%;
 }
 </style>
